2b3e32e7cc7738f1aa6fa002f1e785953b8313c6
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2025-09-17 23:03:28 -0500

    Añadiendo findAll en VoluntarioService

--- src/main/java/com/proyecto/v1/service/VoluntarioService.java
+++ src/main/java/com/proyecto/v1/service/VoluntarioService.java
@@ -3,13 +3,20 @@ package com.proyecto.v1.service;
 import com.proyecto.v1.model.Voluntario;
 import com.proyecto.v1.repository.VoluntarioDao;
 
+import java.util.List;
+
 public class VoluntarioService {
     private final VoluntarioDao voluntarioDao = new VoluntarioDao();
+
 
 
     public Voluntario save(String nombre, String apellido , String nombreUsuario, String correo, String clave) {
 
         Voluntario nuevoVoluntario = new Voluntario(nombre,apellido,nombreUsuario,correo,clave);
         return voluntarioDao.saveVoluntario(nuevoVoluntario);
+    }
+
+    public List<Voluntario> findAllVoluntarios(){
+        return  voluntarioDao.findAll();
     }
 }
