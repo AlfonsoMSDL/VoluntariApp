915eac6bc7457b055b7819ca7a3e2845b86c305a
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2025-09-27 19:18:29 -0500

    Se añade el rol para los dto del voluntario y la organizacion

--- src/main/java/com/proyecto/v1/dto/response/GetOrganizacion.java
+++ src/main/java/com/proyecto/v1/dto/response/GetOrganizacion.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GetOrganizacion(
+        //Estos seran los datos que se mostraran en el cliente
+        @JsonAlias String id,
         @JsonAlias String nombre,
         @JsonAlias String nombreUsuario,
+        @JsonAlias String rol,
         @JsonAlias String correo
 ) {
 }
--- src/main/java/com/proyecto/v1/dto/response/GetVoluntario.java
+++ src/main/java/com/proyecto/v1/dto/response/GetVoluntario.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GetVoluntario(
+        //Estos seran los datos que se mostraran en el cliente
 
         @JsonAlias Long id,
         @JsonAlias String nombre,
         @JsonAlias String apellido,
+        @JsonAlias String rol,
         @JsonAlias String correo,
         @JsonAlias String nombreUsuario
+
 
 ) {
 }
