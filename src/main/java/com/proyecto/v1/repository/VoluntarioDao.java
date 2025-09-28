28bfd955708f5a36923299178269e52b672f0c0a
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2025-09-27 19:11:42 -0500

    Se agregó el metodo para traer un voluntario por su email, ademas ahora se hace el manejo del Rol

--- src/main/java/com/proyecto/v1/repository/VoluntarioDao.java
+++ src/main/java/com/proyecto/v1/repository/VoluntarioDao.java
@@ -1,17 +1,20 @@
 package com.proyecto.v1.repository;
 
 import com.proyecto.v1.model.Voluntario;
+import com.proyecto.v1.model.util.Rol;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class VoluntarioDao {
 
-    private final String INSERT = "INSERT INTO voluntarios (nombre,apellido,nombre_usuario,correo,contrasena) VALUES (?,?,?,?,?)";
+    private final String INSERT = "INSERT INTO voluntarios (nombre,apellido,nombre_usuario,correo,contrasena,rol) VALUES (?,?,?,?,?,?)";
     private final String SELECT = "SELECT * FROM voluntarios";
+    private final String FIND_BY_CORREO = "SELECT * FROM voluntarios WHERE correo = ?";
 
-    public Voluntario saveVoluntario(Voluntario voluntario) {
+    public Voluntario save(Voluntario voluntario) {
 
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -25,6 +28,8 @@ public class VoluntarioDao {
             stmt.setString(3, voluntario.getNombreUsuario());
             stmt.setString(4, voluntario.getCorreo());
             stmt.setString(5, voluntario.getClave());
+            stmt.setString(6, voluntario.getRol().name());
+
             int registrosAfectados = stmt.executeUpdate();
 
             ResultSet rs = stmt.getGeneratedKeys();
@@ -63,8 +68,9 @@ public class VoluntarioDao {
                 String correo =  rs.getString("correo");
                 String clave =  rs.getString("contrasena");
                 String nombreUsuario =  rs.getString("nombre_usuario");
+                Rol rol =  Rol.obtenerRol(rs.getString("rol"));
 
-                voluntario = new Voluntario(idVoluntario,nombre,apellido,correo,clave,nombreUsuario);
+                voluntario = new Voluntario(idVoluntario,nombre,apellido,correo,clave,nombreUsuario,rol);
 
                 voluntarios.add(voluntario);
             }
@@ -77,5 +83,43 @@ public class VoluntarioDao {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Optional<Voluntario> findByCorreo(String correoBuscar){
+        Connection conn;
+        PreparedStatement stmt;
+        Voluntario voluntario = null;
+
+        try{
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(FIND_BY_CORREO);
+            stmt.setString(1, correoBuscar);
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                Long idVoluntario = rs.getLong("id_voluntario");
+                String nombre =  rs.getString("nombre");
+                String apellido =  rs.getString("apellido");
+                String correo = rs.getString("correo");
+                String contrasena =  rs.getString("contrasena");
+                String nombreUsuario =  rs.getString("nombre_usuario");
+
+                Rol rol =  Rol.obtenerRol(rs.getString("rol"));
+
+                voluntario = new Voluntario(idVoluntario,nombre,apellido,correo,contrasena,nombreUsuario,rol);
+
+            }
+
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+
+
+
+            return voluntario != null? Optional.of(voluntario):null; //En caso de que no se encuentre, retorna nulo
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+
     }
 }
