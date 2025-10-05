3110c2aa1b63e05067bd632e258800ee3d3a8f35
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2025-10-05 13:58:17 -0500

    Agregando metodo para actualizar

--- src/main/java/com/proyecto/v2/service/OrganizacionService.java
+++ src/main/java/com/proyecto/v2/service/OrganizacionService.java
@@ -1,6 +1,8 @@
 package com.proyecto.v2.service;
 
 import com.proyecto.v2.model.Organizacion;
+import com.proyecto.v2.model.util.Rol;
+import com.proyecto.v2.model.util.Tipo;
 import com.proyecto.v2.persistence.OrganizacionDao;
 
 import java.util.List;
@@ -9,9 +11,15 @@ import java.util.Optional;
 public class OrganizacionService {
     private final OrganizacionDao organizacionDao = new OrganizacionDao();
 
-    public Organizacion save(String nombre, String nombreUsuario, String correo, String clave){
+    public Organizacion save(String nombre, String nombreUsuario, String correo, String clave,String telefono, String tipoString){
         Organizacion nuevaOrganizacion = new Organizacion(nombre,nombreUsuario,correo,clave);
+
+        Tipo tipo = Tipo.fromString(tipoString);//Traigo un enum dado un string
+        nuevaOrganizacion.setTipo(tipo);
+        nuevaOrganizacion.setTelefono(telefono);
+
         Organizacion insertada = organizacionDao.save(nuevaOrganizacion);
+
         return insertada;
     }
 
@@ -20,4 +28,16 @@ public class OrganizacionService {
         return organizacionDao.findAll();
     }
     public Optional<Organizacion> findByCorreo(String correo){return  organizacionDao.findByCorreo(correo);}
+
+    public Organizacion update(Long id, String nombre, String correo, String telefono, String clave, String tipo, String descripcion, String nombreUsuario){
+
+        if(clave == null || clave.trim().isEmpty()){
+            //No se proporciono una clave, por lo tanto, se deja la misma de antes
+            Organizacion organizacionActual =  organizacionDao.findById(id).get();
+            clave = organizacionActual.getClave();
+        }
+
+        Organizacion organizacionUpdate = new Organizacion(id,nombre,correo,telefono,clave,nombreUsuario,Tipo.fromString(tipo),descripcion);
+        return organizacionDao.update(organizacionUpdate);
+    }
 }
--- src/main/java/com/proyecto/v2/service/VoluntarioService.java
+++ src/main/java/com/proyecto/v2/service/VoluntarioService.java
@@ -1,5 +1,6 @@
 package com.proyecto.v2.service;
 
+import com.proyecto.v2.model.Organizacion;
 import com.proyecto.v2.model.Voluntario;
 import com.proyecto.v2.persistence.VoluntarioDao;
 
@@ -9,9 +10,10 @@ import java.util.Optional;
 public class VoluntarioService {
     private final VoluntarioDao voluntarioDao = new VoluntarioDao();
 
-    public Voluntario save(String nombre, String apellido , String nombreUsuario, String correo, String clave) {
+    public Voluntario save(String nombre, String apellido , String nombreUsuario, String correo, String clave,String telefono) {
 
         Voluntario nuevoVoluntario = new Voluntario(nombre,apellido,nombreUsuario,correo,clave);
+        nuevoVoluntario.setTelefono(telefono);
         Voluntario insertado = voluntarioDao.save(nuevoVoluntario);
         return insertado;
     }
@@ -21,5 +23,18 @@ public class VoluntarioService {
     }
     public Optional<Voluntario> findByCorreo(String correo){
         return voluntarioDao.findByCorreo(correo);
+    }
+
+    public Voluntario update(Long id, String nombre, String apellido, String correo, String telefono, String clave, String habilidades, String experiencia, String disponibilidad, String areas_interes, String nombreUsuario){
+
+        if(clave == null || clave.trim().isEmpty()){
+            //No se proporciono una clave, por lo tanto, se deja la misma de antes
+            Voluntario voluntarioActual =  voluntarioDao.findById(id).get();
+            clave = voluntarioActual.getClave();
+        }
+
+        Voluntario voluntarioActualizar = new Voluntario(id,nombre,apellido,correo,telefono,clave,habilidades,experiencia,disponibilidad,areas_interes,nombreUsuario);
+
+        return  voluntarioDao.update(voluntarioActualizar);
     }
 }
