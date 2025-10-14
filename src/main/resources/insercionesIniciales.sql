6e1b2e7c55d1e6ef3c42b1ea2a36a1b0bec5cfe1
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2025-10-13 20:04:12 -0500

    Inserciones iniciales para hacer pruebas

---
+++ src/main/resources/insercionesIniciales.sql
@@ -0,0 +1,16 @@
+INSERT INTO tipo_organizacion (tipo, descripcion) VALUES
+                                                      ('Ambiental', 'Organización dedicada a la protección del medio ambiente, reforestación y limpieza de espacios públicos.'),
+                                                      ('Educativa', 'Entidad que promueve la educación gratuita, tutorías y alfabetización en comunidades vulnerables.'),
+                                                      ('Salud Comunitaria', 'Grupo que realiza jornadas médicas, campañas de vacunación y promoción de hábitos saludables.'),
+                                                      ('Animalista', 'Organización orientada al rescate, adopción y bienestar de animales en situación de calle.'),
+                                                      ('Cultural', 'Asociación que impulsa actividades artísticas, festivales locales y preservación de la cultura regional.'),
+                                                      ('Juvenil', 'Colectivo enfocado en el liderazgo y participación de jóvenes en proyectos de desarrollo comunitario.'),
+                                                      ('Social y Humanitaria', 'Organización que brinda apoyo a personas en situación de pobreza o emergencias.'),
+                                                      ('Deportiva Comunitaria', 'Grupo que organiza actividades deportivas para fomentar la integración y la salud física.'),
+                                                      ('Tecnológica y de Innovación Social', 'Iniciativa que busca acercar la tecnología y la capacitación digital a comunidades con bajos recursos.'),
+                                                      ('Voluntariado Religioso', 'Grupo de servicio social promovido por instituciones religiosas para apoyar causas solidarias.');
+
+INSERT INTO roles (nombre, descripcion) VALUES
+                                            ("Voluntario","Usuario que quiere ayudar"),
+                                            ("Organizacion","Entidad que publica proyectos comunitarios"),
+                                            ("Administrador","Gestiona la plataforma");
